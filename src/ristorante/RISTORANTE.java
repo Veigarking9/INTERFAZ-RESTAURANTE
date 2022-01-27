bda3c5cf2cd4cc703618b87f917d71b214a0b24a
xabier.barreiroalber <xabier.barreiroalber@EQUIP077238.xunta.local>
2022-01-27 17:49:45 +0100

    COMPLETO

---
+++ src/ristorante/RISTORANTE.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ristorante;
+
+/**
+ *
+ * @author xabier.barreiroalber
+ */
+public class RISTORANTE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       VentanaP ventana = new VentanaP();
+       ventana.setTitle("VENTANA PEDIDO");
+       ventana.setVisible(true);
+    }
+    
+}
